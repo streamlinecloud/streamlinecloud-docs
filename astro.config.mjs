396bf90e2c39606bf7470711fa67448a82c3d1be
quinilo <hiquenvol@gmail.com>
2025-08-28 14:38:49 +0200

    Custom css streamline theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'StreamlineCloud',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'StreamlineCloud',
-					// Autogenerate a group of links for the 'constellations' directory.
-					autogenerate: { directory: 'streamline' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'StreamlineCloud',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          customCss: [
+              // Relative path to your custom CSS file
+              './src/styles/streamline.css',
+          ],
+          sidebar: [
+              {
+                  label: 'StreamlineCloud',
+                  // Autogenerate a group of links for the 'constellations' directory.
+                  autogenerate: { directory: 'streamline' },
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
